ddf2feb4ee8bf339490c349d44f5f04617c151ee
Adrian Stefan <92085719+AdrianST19@users.noreply.github.com>
2022-05-01 15:47:06 +0300

    Add files via upload

---
+++ Pacman.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pacmangame;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Adrian Stefan
+ */
+public class Pacman extends JFrame{
+
+	public Pacman() {
+		add(new Model());
+	}
+	
+	
+	public static void main(String[] args) {
+		Pacman pac = new Pacman();
+		pac.setVisible(true);
+		pac.setTitle("Pacman");
+		pac.setSize(380,420);
+		pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pac.setLocationRelativeTo(null);
+		
+	}
+
+}
